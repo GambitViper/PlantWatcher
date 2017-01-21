d5fb922b9e71fb4df5893f2d93523493fa835f6e
GambitViper <baklund.zachary@uwlax.edu>
2017-01-21 14:04:05 -0600

    Redo Views with new props in plantgroup

--- ImagineCupBTrees/Models/Plantgroup.cs
+++ ImagineCupBTrees/Models/Plantgroup.cs
@@ -11,11 +11,18 @@ namespace ImagineCupBTrees.Models
         [Key]
         public int Id { get; set; }
         public string PlantName { get; set; }
+        public double TemperatureF { get; set; }
+        public double Humidity { get; set; }
+        public double Lux { get; set; }
+        public double SoilMoisture { get; set; }
+        public bool FoggerOn { get; set; }
+        public bool BoilerOn { get; set; }
+        public bool BonsaiOn { get; set; }
         //public Light Light { get; set; }
         //public Soil Soil { get; set; }
         //public Temp Temp { get; set; }
         //public Humidity Humidity { get; set; }
-        public bool AutoDeviceManagement { get; set; }
+        
 
     }
 }
